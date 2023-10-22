2077b1702077fa355c6558056ca4a99c73c73b48
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2023-10-23 02:01:16 +0300

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,77 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Statistics from './Statistics/Statistics';
 import Feedback from './Feedback/Feedback';
 import Notification from './Notification/Notification';
 
-export class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  // const [good, setGood] = useState(0);
-  // const [neutral, setNeutral] = useState(0);
-  // const [bad, setBad] = useState(0);
+export function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  //  onClickButtons(evt) {
-  //   if (evt === 'Good') {
-  //     setGood(evt => evt + 1);
-  //   }
-  //   if (evt === 'Neutral') {
-  //     setNeutral(evt => evt + 1);
-  //   }
-  //   if (evt === 'Bad') {
-  //     setBad(evt => evt + 1);
-  //   }
-  // }
-  countTotalFeedback = () => {
-    const { good, bad, neutral } = this.state;
-    return good + bad + neutral;
-  };
-  countPositiveFeedbackPercentage = total => {
-    return Math.floor((this.state.good / total) * 100);
-  };
-  onLeaveFeedback = btnName => {
-    switch (btnName) {
-      case 'good':
-        this.setState(prev => ({ good: prev.good + 1 }));
-        break;
-      case 'bad':
-        this.setState(prev => ({ bad: prev.bad + 1 }));
-        break;
-      case 'neutral':
-        this.setState(prev => ({ neutral: prev.neutral + 1 }));
-        break;
-      default:
-        break;
+  function onClickButtons(e) {
+    if (e === 'Good') {
+      setGood(e => e + 1);
     }
-  };
-
-  render() {
-    const { good, bad, neutral } = this.state;
-    const total = this.countTotalFeedback();
-    const percent = this.countPositiveFeedbackPercentage(total);
-    return (
-      <div className="container">
-        <div className="wrapper">
-          <p>Please leave feedback</p>
-          <Feedback
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.onLeaveFeedback}
-          />
-          {total !== 0 ? (
-            <Statistics
-              good={good}
-              bad={bad}
-              neutral={neutral}
-              total={total}
-              percent={percent}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </div>
-      </div>
-    );
+    if (e === 'Neutral') {
+      setNeutral(e => e + 1);
+    }
+    if (e === 'Bad') {
+      setBad(e => e + 1);
+    }
   }
+
+  const total = good + bad + neutral;
+  const percent = Math.floor((good / total) * 100);
+
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <p>Please leave feedback</p>
+        <Feedback onClickButtons={onClickButtons} />
+        {total !== 0 ? (
+          <Statistics
+            good={good}
+            bad={bad}
+            neutral={neutral}
+            total={total}
+            percent={percent}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </div>
+    </div>
+  );
 }
+
